852ec9245c5947e379305239054d663140861f9c
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-08-28 23:52:09 +0200

    Create the orders model and provider class

---
+++ lib/provider/orders_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:grocery_app/models/orders_model.dart';
+
+class OrdersProvider with ChangeNotifier {
+  static List<OrderModel> _orders = [];
+  List<OrderModel> get getOrders {
+    return _orders;
+  }
+}
