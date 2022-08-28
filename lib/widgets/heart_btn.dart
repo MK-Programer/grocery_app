72d05c3f81251ce6defac6a46f498f894094bbc4
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-08-28 21:56:01 +0200

    Show a progress indicator while adding a product to the wishlist

--- lib/widgets/heart_btn.dart
+++ lib/widgets/heart_btn.dart
@@ -8,9 +8,10 @@ import 'package:provider/provider.dart';
 import '../provider/products_provider.dart';
 import '../services/utils.dart';
 
-class HeartBTN extends StatelessWidget {
+class HeartBTN extends StatefulWidget {
   final String productId;
   final bool? isInWishList;
+
   const HeartBTN({
     Key? key,
     required this.productId,
@@ -18,46 +19,68 @@ class HeartBTN extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<HeartBTN> createState() => _HeartBTNState();
+}
+
+class _HeartBTNState extends State<HeartBTN> {
+  bool _isLoading = false;
+  @override
   Widget build(BuildContext context) {
     final Color color = Utils(context).color;
     final wishListProvider = Provider.of<WishListProvider>(context);
     final user = authInstance.currentUser;
     final productProvider = Provider.of<ProductsProvider>(context);
-    final getCurrentProduct = productProvider.findProdById(productId);
+    final getCurrentProduct = productProvider.findProdById(widget.productId);
     return GestureDetector(
       onTap: () async {
         // wishListProvider.addRemoveProductsToWishList(productId: productId);
+        setState(() => _isLoading = true);
         try {
           if (user == null) {
+            // setState(() => _isLoading = false);
             GlobalMethods.errorDialog(
                 subTitle: "No user found, Please login first",
                 context: context);
             return;
           }
-          if (isInWishList == false && isInWishList != null) {
+          if (widget.isInWishList == false && widget.isInWishList != null) {
             await GlobalMethods.addToWishlist(
-              productId: productId,
+              productId: widget.productId,
               context: context,
             );
           } else {
             await wishListProvider.removeOneItem(
               wishlistId:
                   wishListProvider.getwishListItems[getCurrentProduct.id]!.id,
-              productId: productId,
+              productId: widget.productId,
             );
           }
           await wishListProvider.fetchWishlist();
+          setState(() => _isLoading = false);
         } catch (error) {
-        } finally {}
+          GlobalMethods.errorDialog(subTitle: '$error', context: context);
+        } finally {
+          setState(() => _isLoading = false);
+        }
       },
-      child: Icon(
-        isInWishList != null && isInWishList == true
-            ? IconlyBold.heart
-            : IconlyLight.heart,
-        size: 22,
-        color:
-            isInWishList != null && isInWishList == true ? Colors.red : color,
-      ),
+      child: _isLoading
+          ? const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 20.0,
+                width: 20.0,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Icon(
+              widget.isInWishList != null && widget.isInWishList == true
+                  ? IconlyBold.heart
+                  : IconlyLight.heart,
+              size: 22,
+              color: widget.isInWishList != null && widget.isInWishList == true
+                  ? Colors.red
+                  : color,
+            ),
     );
   }
 }
